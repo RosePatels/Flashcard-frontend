304987b4e72a2c74eb899d28729714d2bec459e5
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2025-12-23 20:49:33 -0500

    Setup tailwind variables, logo, css reset

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    test
+  <div class="app-full-height bg-(--color-figma-neutral-100) py-10 flex">
+    <div class="py-5">
+      <img src="./assets/logo.png" alt="Logo" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-
+.app-full-height {
+  height: 100vh;
+}
 </style>
