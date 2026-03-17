8626fc5abe9bc9fae7f4dfed40bb6d2eaaafaa63
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2026-03-16 21:11:24 -0400

    Create container for study statistics

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './views/main-header/Navbar.vue'
 import FlashcardMainContainer from './views/flashcard/FlashcardMainContainer.vue';
+import StudyStatisticsContainer from './views/study-statistics/StudyStatisticsContainer.vue';
 </script>
 
 <template>
-  <div class="app-full-height bg-(--color-figma-neutral-100) py-(--figma-spacing-300) px-(--figma-spacing-1200)">
-    <Navbar />
-    <div class="grid grid-cols-12 mt-(--figma-spacing-300) lg:mt-(--figma-spacing-400)">
-      <FlashcardMainContainer class="col-span-12 lg:col-span-8" />
+  <div class="h-[100vh] w-full bg-(--color-figma-neutral-100)">
+    <div class="app-full-height m-auto py-(--figma-spacing-300) px-(--figma-spacing-1200)">
+      <Navbar />
+      <div class="grid grid-cols-12 mt-(--figma-spacing-300) lg:mt-(--figma-spacing-400) gap-(--figma-spacing-300) lg:gap-(--figma-spacing-400)">
+        <FlashcardMainContainer class="col-span-12 lg:col-span-8" />
+        <StudyStatisticsContainer class="col-span-12 lg:col-span-4 mt-(--figma-spacing-300) lg:mt-0" />
+      </div>
     </div>
   </div>
 </template>
@@ -16,5 +20,6 @@ import FlashcardMainContainer from './views/flashcard/FlashcardMainContainer.vue
 <style scoped>
 .app-full-height {
   height: 100vh;
+  max-width: 1200px;
 }
 </style>
---
+++ src/views/study-statistics/StudyStatisticsContainer.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="study-statistics-container">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.study-statistics-container {
+    background-color: white;
+    border-left: 1px solid var(--color-figma-neutral-900);
+    border-top: 1px solid var(--color-figma-neutral-900);
+    border-right: 3px solid var(--color-figma-neutral-900);
+    border-bottom: 3px solid var(--color-figma-neutral-900);
+    border-radius: 16px;
+}
+
+</style>
