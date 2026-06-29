f32532c9601df426c757503f20d34203d27b54c3
Shree Patel <36641237+RosePatels@users.noreply.github.com>
2026-06-28 22:53:16 -0400

    Update Study Statistics to use real flashcard data

--- src/views/study-statistics/StatisticsCard.vue
+++ src/views/study-statistics/StatisticsCard.vue
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-4 border-1 border-solid border-(--color-figma-neutral-900) rounded-(--figma-spacing-150) statistics-card mb-(--figma-spacing-250)">
         <div class="col-span-3 border-r-1 border-solid border-(--color-figma-neutral-900))) p-(--figma-spacing-250) ">
             <h3 class="text-preset-4">Total Cards</h3>
-            <div class="text-preset-1">40</div>
+            <div class="text-preset-1">{{ stateStore.flashcards.length }}</div>
         </div>
         <div class="flex items-center justify-center col-span-1 bg-(--color-blue-400) rounded-r-(--figma-spacing-125)">
             <img class="hidden sm:block" src="../../assets/images/icon-stats-total.svg" alt="Logo" />
@@ -12,7 +12,7 @@
      <div class="grid grid-cols-4 border-1 border-solid border-(--color-figma-neutral-900) rounded-(--figma-spacing-150) statistics-card mb-(--figma-spacing-250)">
         <div class="col-span-3 border-r-1 border-solid border-(--color-figma-neutral-900))) p-(--figma-spacing-250) ">
             <h3 class="text-preset-4">Mastered</h3>
-            <div class="text-preset-1">11</div>
+            <div class="text-preset-1">{{ getMasteredCount() }}</div>
         </div>
         <div class="flex items-center justify-center col-span-1 bg-(--color-teal-400) rounded-r-(--figma-spacing-125)">
             <img class="hidden sm:block" src="../../assets/images/icon-stats-mastered.svg" alt="Logo" />
@@ -22,7 +22,7 @@
      <div class="grid grid-cols-4 border-1 border-solid border-(--color-figma-neutral-900) rounded-(--figma-spacing-150) statistics-card mb-(--figma-spacing-250)">
         <div class="col-span-3 border-r-1 border-solid border-(--color-figma-neutral-900))) p-(--figma-spacing-250) ">
             <h3 class="text-preset-4">In Progress</h3>
-            <div class="text-preset-1">21</div>
+            <div class="text-preset-1">{{ getInProgressCount() }}</div>
         </div>
         <div class="flex items-center justify-center col-span-1 bg-(--color-pink-500) rounded-r-(--figma-spacing-125)">
             <img class="hidden sm:block" src="../../assets/images/icon-stats-in-progress.svg" alt="Logo" />
@@ -32,7 +32,7 @@
      <div class="grid grid-cols-4 border-1 border-solid border-(--color-figma-neutral-900) rounded-(--figma-spacing-150) statistics-card">
         <div class="col-span-3 border-r-1 border-solid border-(--color-figma-neutral-900))) p-(--figma-spacing-250) ">
             <h3 class="text-preset-4">Not Started</h3>
-            <div class="text-preset-1">8</div>
+            <div class="text-preset-1">{{ getNotStartedCount() }}</div>
         </div>
         <div class="flex items-center justify-center col-span-1 bg-(--color-pink-400) rounded-r-(--figma-spacing-125)">
             <img class="hidden sm:block" src="../../assets/images/icon-stats-not-started.svg" alt="Logo" />
@@ -41,7 +41,21 @@
 </template>
 
 <script setup lang="ts">
+import { useStateStore } from '@/stores/state';
 
+const stateStore = useStateStore();
+
+const getMasteredCount = () => {
+    return stateStore.flashcards.filter(card => card.masteryProgress === 5).length;
+}
+
+const getInProgressCount = () => {
+    return stateStore.flashcards.filter(card => card.masteryProgress > 0 && card.masteryProgress < 5).length;
+}
+
+const getNotStartedCount = () => {
+    return stateStore.flashcards.filter(card => card.masteryProgress === 0).length;
+}
 </script>
 
 <style scoped>
